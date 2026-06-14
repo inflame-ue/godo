20a59bbad1e25cb3af5727a1f9430b8e696a5023
Ilia Oberemok <i.oberemok777@gmail.com>
2026-06-14 07:55:40 +0300

    internal/api: implement the TODO api with five endpoints exposing CRUD ops

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/inflame-ue/godo/internal/api"
 	"github.com/inflame-ue/godo/internal/database"
 	"github.com/joho/godotenv"
 )
@@ -17,10 +20,19 @@ func main() {
 
 	uri := os.Getenv("MONGODB_URI")
 	dbName := os.Getenv("MONGODB_NAME")
-	_, err = database.NewMongoClient(context.Background(), uri, dbName)
+	db, err := database.NewMongoClient(context.Background(), uri, dbName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("successfully initialized and pinged deployment")
+	port := os.Getenv("PORT")
+	srv := api.NewAPI(db)
+	serv := http.Server{
+		Addr:              ":" + port,
+		Handler:           srv,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+	}
+	log.Printf("server listening on port %s", port)
+	log.Fatal(serv.ListenAndServe())
 }
